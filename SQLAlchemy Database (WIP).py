510c0afda635e327772ca53bc662375b98b157b0
Paul James M <pmabalot2004@gmail.com>
2024-05-20 23:15:53 +0100

    fixed "users" table error

--- SQLAlchemy Database (WIP).py
+++ SQLAlchemy Database (WIP).py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "this is a secret key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.splite3"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes = 5)
 
@@ -80,5 +80,5 @@ def logout():
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all
+        db.create_all()
         app.run(debug = True)
